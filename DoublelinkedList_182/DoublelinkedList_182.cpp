8197fa93d199f7cae4a04f7471210c5a157e8574
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-05-26 10:58:44 +0700

    5

--- DoublelinkedList_182/DoublelinkedList_182.cpp
+++ DoublelinkedList_182/DoublelinkedList_182.cpp
@@ -9,3 +9,15 @@ public:
 	Node* next;
 	Node* prev;
 };
+
+class DoubleLinkedList {
+private:
+	Node* START;
+public:
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void traverseO();
+};
